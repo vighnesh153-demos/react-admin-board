851f2eb4b25e55a9920662f5a559463fb30c0340
vighnesh153 <vighnesh.raut13@gmail.com>
2020-09-12 10:29:26 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {Admin, Resource} from 'react-admin';
 import {UserList} from './users';
 import {PostList, PostCreate, PostEdit} from "./posts";
 
+import jsonServerProvider from 'ra-data-json-server';
+
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
-import jsonServerProvider from 'ra-data-json-server';
 import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
@@ -16,6 +18,7 @@ function App() {
   return (
     <div className="App">
       <Admin
+        authProvider={authProvider}
         dashboard={Dashboard}
         dataProvider={dataProvider}
       >
